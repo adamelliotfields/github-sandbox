8225e3490a0a92f17d5f2870264edef37eebfe3f
Adam Fields <adam.elliot.fields@gmail.com>
2020-03-18 14:13:12 -0400

    feat(subtract): #1 Add `subtract` method (#8)
    * feat(subtract): Add `subtract` method.
    * chore(dependabot): Update Dependabot config.

---
+++ src/lib/subtract.ts
@@ -0,0 +1,8 @@
+/**
+ * @param {number} minuend
+ * @param {number} subtrahend
+ * @returns {number}
+ */
+export default function add(minuend: number, subtrahend: number): number {
+  return minuend - subtrahend;
+}
---
+++ src/test/subtract.test.ts
@@ -0,0 +1,5 @@
+import subtract from '../lib/subtract';
+
+test('subtracts 2 and 2', () => {
+  expect(subtract(2, 2)).toEqual(0);
+});
