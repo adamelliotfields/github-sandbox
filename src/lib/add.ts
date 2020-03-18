fe3bb195b7dc16d38b7ec635696a3d3a6d0d76ba
Adam Fields <adam.elliot.fields@gmail.com>
2020-03-18 11:55:16 -0400

    feat(add): Add `add` method.

---
+++ src/lib/add.ts
@@ -0,0 +1,8 @@
+/**
+ * @param {number} augend
+ * @param {number} addend
+ * @returns {number}
+ */
+export default function add(augend: number, addend: number): number {
+  return augend + addend;
+}
---
+++ src/test/add.test.ts
@@ -0,0 +1,5 @@
+import add from '../lib/add';
+
+test('adds 2 and 2', () => {
+  expect(add(2, 2)).toEqual(4);
+});
